469e7909356dea6bd31ddaa3b633d96fe2773627
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-11 14:03:08 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,9 +15,9 @@ def third_challenge
         status : {"alive"}
       },
       :hero_friends => [
-        name
-        age
-        attitude
+        name : {},
+        age : {},
+        attitude : {}
        ]
    }, 
    :capulet => {
