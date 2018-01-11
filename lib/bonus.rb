4e21f9b64b13af45963ca7ef3bae40bd39d77fee
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-11 14:43:10 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,9 +22,6 @@ def bonus
 
   epic_tragedy[:Montague][:hero][:status] = "alive"
 
-  
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
 
